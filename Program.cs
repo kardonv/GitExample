d49afbe4299cbbfd0c634bf429e89f3a99a4edf0
Volodymyr <vladimir.vinohradov@outlook.com>
2024-07-18 19:14:30 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GitExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadLine();
+        }
+    }
+}
