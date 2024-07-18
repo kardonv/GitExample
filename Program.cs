a5e99a0d55420d4e1b216497cece7e9d896970bf
Volodymyr <vladimir.vinohradov@outlook.com>
2024-07-18 19:23:14 +0300

    Added logic to print rectangle

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@
         {
             Console.WriteLine("Hello, World!");
 
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
+
             Console.ReadLine();
         }
     }
